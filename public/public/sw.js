5d0c9e05b34054af932584f48184825b1e8ca2c2
Gabriel Halicki Shuchmacher Silva <gabrielshuchmachersilva@gmail.com>
2026-03-30 23:39:39 -0300

    Delete public/public directory

--- public/public/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Instalado');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Estratégia básica: apenas deixa passar as requisições
-  e.respondWith(fetch(e.request));
-});
